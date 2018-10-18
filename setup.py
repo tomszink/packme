bcedd4dac2b5d8ccadb55c2641dc39375604ca6b
Thomas Zink <toms.zink@gmail.com>
2018-10-18 21:55:42 +0200

    Add test-requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='packme',
-    description='A minimal yet meaningful project setup',
+    description='A minimal yet meaningful Python project setup',
     author='tomszink',
     package_dir={"": "src"},  # for all packages, look into ./src/*
     packages=['packme'],
