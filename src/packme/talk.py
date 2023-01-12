09305c018724f1f48d67f0266eec174f547e7c84
Toms Zink <toms.zink@gmail.com>
2023-01-12 21:16:44 +0100

    Fix flake8 error

--- src/packme/talk.py
+++ src/packme/talk.py
@@ -1,3 +1,3 @@
 # can be called by: packme.talk.say_hello_to("name")
 def say_hello_to(name):
-    return(f"Hello, dear { name }, nice to see you!")
+    return (f"Hello, dear { name }, nice to see you!")
